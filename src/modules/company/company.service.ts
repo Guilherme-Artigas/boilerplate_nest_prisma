b91e124f3f833a7e6a770f2706fa2eee50887ba5
Matheus Sena <senamatheusjob@gmail.com>
2025-05-20 19:30:07 -0300

    feat: check if manager exists before create company

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -1,7 +1,8 @@
-import { HttpException, HttpStatus, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { IPaginationOptions, paginate } from 'nestjs-typeorm-paginate';
 import { Company } from 'src/entities/company.entity';
+import { Manager } from 'src/entities/manager.entity';
 import { CreateCompanyDto } from 'src/modules/company/dto/create-company.dto';
 import { UpdateCompanyDto } from 'src/modules/company/dto/update-company.dto';
 import { DataSource, Repository } from 'typeorm';
@@ -10,17 +11,23 @@ import { DataSource, Repository } from 'typeorm';
 export class CompanyService {
     private readonly logger = new Logger(CompanyService.name);
     private readonly companyRepository: Repository<Company>;
+    private readonly managerRepository: Repository<Manager>;
 
     constructor(@InjectDataSource() dataSource: DataSource) {
         this.companyRepository = dataSource.getRepository(Company);
+        this.managerRepository = dataSource.getRepository(Manager);
     }
 
     async createCompany(createCompanyDto: CreateCompanyDto) {
         try {
             this.logger.log(`Creating company with name: ${createCompanyDto.name}`);
-            const company = await this.companyRepository.save({ name: createCompanyDto.name })
-            this.logger.log(`Company created successfully with ID: ${company.id}`);
-            return company;
+            const company = await this.companyRepository.findOne({ where: { name: createCompanyDto.name } })
+            if (company) throw new BadRequestException('This company already exists')
+            const manager = await this.managerRepository.findOne({ where: { id: createCompanyDto.managerId } })
+            if (!manager) throw new NotFoundException('Manager not found')
+            const newCompany = await this.companyRepository.save({ name: createCompanyDto.name, manager })
+            this.logger.log(`Company created successfully with ID: ${newCompany.id}`);
+            return newCompany;
         } catch (error) {
             this.logger.error(`Failed to create company with NAME: ${createCompanyDto.name}. Error: ${error.message}.`);
             throw new HttpException(error.message, error.status || HttpStatus.INTERNAL_SERVER_ERROR);
@@ -57,9 +64,14 @@ export class CompanyService {
             this.logger.log(`Updating company with ID: ${companyId}`);
             const company = await this.companyRepository.findOne({ where: { id: companyId } });
             if (!company) throw new NotFoundException('Company not found')
-            await this.companyRepository.save({ ...company, ...updateCompanyDto });
+            if (updateCompanyDto.managerId) {
+                const manager = await this.managerRepository.findOne({ where: { id: updateCompanyDto.managerId } })
+                if (!manager) throw new NotFoundException('Manager not found')
+                updateCompanyDto['manager'] = manager;
+            }
+            const updatedCompany = await this.companyRepository.save({ ...company, ...updateCompanyDto });
             this.logger.log(`Company with ID ${companyId} updated successfully`);
-            return { id: company.id };
+            return updatedCompany;
         } catch (error) {
             this.logger.error(`Failed to update company with ID: ${companyId}. Error: ${error.message}.`);
             throw new HttpException(error.message, error.status || HttpStatus.INTERNAL_SERVER_ERROR);
