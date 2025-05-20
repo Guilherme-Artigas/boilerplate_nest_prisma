8e1a684a9176e00dbca6217e690dc3004aa2aac8
Matheus Sena <senamatheusjob@gmail.com>
2025-05-20 12:53:28 -0300

    feat: create manager entity

---
+++ src/entities/manager.entity.ts
@@ -0,0 +1,42 @@
+import { Company } from 'src/entities/company.entity';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+    Unique,
+    Index,
+    OneToMany,
+} from 'typeorm';
+
+@Entity('users')
+@Unique(['email', 'cpf'])
+export class Manager {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('varchar', { length: 250, nullable: false })
+    name: string;
+
+    @Index()
+    @Column('varchar', { length: 100, nullable: true })
+    email: string;
+
+    @Column('varchar', { length: 20, nullable: true })
+    phone: string;
+
+    @Index()
+    @Column('varchar', { length: 14, nullable: false })
+    cpf: string;
+
+    @OneToMany(() => Company, (company) => company.manager, { lazy: true })
+    company: Company[];
+
+    @CreateDateColumn({ type: 'datetime' })
+    createdAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
