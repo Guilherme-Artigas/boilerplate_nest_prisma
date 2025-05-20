4e6502d4c77e1ee7c990ac8d766c3f8b7fb7d46a
Matheus Sena <senamatheusjob@gmail.com>
2025-05-20 19:30:39 -0300

    add example on UpdateCompanyDto

--- src/modules/company/dto/update-company.dto.ts
+++ src/modules/company/dto/update-company.dto.ts
@@ -1,15 +1,15 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, IsUUID, Length, } from 'class-validator';
 
 export class UpdateCompanyDto {
-    @ApiPropertyOptional()
+    @ApiProperty({ example: 'Any Company', description: 'Company name', nullable: true, required: false })
     @IsOptional()
     @IsString()
     @IsNotEmpty()
     @Length(3)
     name: string;
 
-    @ApiPropertyOptional()
+    @ApiProperty({ example: '94bd055e-4c0d-4a06-888a-600701e3e9a8', description: 'Manager Id', nullable: true, required: false })
     @IsOptional()
     @IsUUID()
     @IsString()
