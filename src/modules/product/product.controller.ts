a493e2a220a5e2f4c8d8667a64facf901aa8f736
Matheus <ymatheus.h@gmail.com>
2025-05-23 06:24:42 -0300

    feat: enhance ProductController and ProductService with detailed API responses and error handling

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,36 +1,95 @@
-import type { CreateProductDto } from '@modules/product/dto/create-product.dto';
-import type { UpdateProductDto } from '@modules/product/dto/update-product.dto';
+import { IsPublic } from '@modules/auth/decorators/is-public.decorator';
+import { CreateProductDto } from '@modules/product/dto/create-product.dto';
+import {
+  ResponseCreateProductDto,
+  ResponseDeleteProductDto,
+  ResponseGetProductDto,
+  ResponsePaginatedProductsDto,
+  ResponseUpdateProductDto,
+} from '@modules/product/dto/response-product.dto';
+import { UpdateProductDto } from '@modules/product/dto/update-product.dto';
 import { ProductService } from '@modules/product/product.service';
-import { Controller, Get, Post, Body, Param, Delete, ParseIntPipe, Patch } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseIntPipe,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { QueryPaginationDto } from 'src/dtos/query-pagination.dto';
 
 @ApiTags('Produtos')
 @Controller('products')
 export class ProductController {
   constructor(private readonly service: ProductService) {}
 
+  @IsPublic()
   @Post()
-  create(@Body() data: CreateProductDto) {
+  @ApiBody({ type: CreateProductDto })
+  @ApiOperation({ summary: 'Criar produto.' })
+  @ApiOkResponse({ status: 201, type: ResponseCreateProductDto })
+  @ApiBadRequestResponse({ description: 'Requisição inválida.' })
+  @ApiNotFoundResponse({ description: 'Empresa não encontrada.' })
+  @ApiInternalServerErrorResponse({ description: 'Erro interno no servidor.' })
+  create(@Body() data: CreateProductDto): Promise<ResponseCreateProductDto> {
     return this.service.create(data);
   }
 
+  @IsPublic()
   @Get()
-  findAll() {
-    return this.service.findAll();
+  @ApiOperation({ summary: 'Listar todos os produtos.' })
+  @ApiOkResponse({ status: 200, type: ResponsePaginatedProductsDto })
+  @ApiInternalServerErrorResponse({ description: 'Erro interno no servidor.' })
+  findAll(@Query() query: QueryPaginationDto): Promise<ResponsePaginatedProductsDto> {
+    return this.service.findAll(query);
   }
 
+  @IsPublic()
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  @ApiOperation({ summary: 'Buscar produto por ID.' })
+  @ApiParam({ name: 'id', required: true, type: Number })
+  @ApiOkResponse({ status: 200, type: ResponseGetProductDto })
+  @ApiNotFoundResponse({ description: 'Produto não encontrado.' })
+  @ApiBadRequestResponse({ description: 'ID inválido.' })
+  @ApiInternalServerErrorResponse({ description: 'Erro interno no servidor.' })
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<ResponseGetProductDto> {
     return this.service.findOne(id);
   }
 
+  @IsPublic()
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateProductDto) {
-    return this.service.update(id, dto);
+  @ApiBody({ type: UpdateProductDto })
+  @ApiOperation({ summary: 'Atualizar produto.' })
+  @ApiOkResponse({ status: 200, type: ResponseUpdateProductDto })
+  @ApiNotFoundResponse({ description: 'Produto não encontrado.' })
+  @ApiBadRequestResponse({ description: 'Requisição inválida.' })
+  @ApiInternalServerErrorResponse({ description: 'Erro interno no servidor.' })
+  update(@Param('id', ParseIntPipe) id: number, @Body() data: UpdateProductDto) {
+    return this.service.update(id, data);
   }
 
+  @IsPublic()
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
+  @ApiOperation({ summary: 'Excluir produto.' })
+  @ApiOkResponse({ status: 200, type: ResponseDeleteProductDto })
+  @ApiNotFoundResponse({ description: 'Produto não encontrado.' })
+  @ApiBadRequestResponse({ description: 'ID inválido.' })
+  @ApiInternalServerErrorResponse({ description: 'Erro interno no servidor.' })
+  remove(@Param('id', ParseIntPipe) id: number): Promise<ResponseDeleteProductDto> {
     return this.service.remove(id);
   }
 }
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,15 +1,33 @@
 import { PrismaService } from '@database/PrismaService';
 import type { CreateProductDto } from '@modules/product/dto/create-product.dto';
+import {
+  ResponseCreateProductDto,
+  ResponseDeleteProductDto,
+  ResponseGetProductDto,
+  ResponsePaginatedProductsDto,
+  ResponseUpdateProductDto,
+} from '@modules/product/dto/response-product.dto';
 import type { UpdateProductDto } from '@modules/product/dto/update-product.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { productToDto } from '@utils/product-to-dto';
+import { QueryPaginationDto } from 'src/dtos/query-pagination.dto';
 
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
-  create(dto: CreateProductDto) {
+  async create(dto: CreateProductDto): Promise<ResponseCreateProductDto> {
     const { companyId, ...rest } = dto;
-    return this.prisma.product.create({
+
+    const companyExists = await this.prisma.company.findUnique({
+      where: { id: companyId },
+    });
+
+    if (!companyExists) {
+      throw new NotFoundException('Empresa não encontrada.');
+    }
+
+    const product = await this.prisma.product.create({
       data: {
         ...rest,
         company: {
@@ -17,20 +35,80 @@ export class ProductService {
         },
       },
     });
-  }
-  findAll() {
-    return this.prisma.product.findMany();
+
+    return productToDto(product);
   }
 
-  findOne(id: number) {
-    return this.prisma.product.findUnique({ where: { id } });
+  async findAll(query: QueryPaginationDto): Promise<ResponsePaginatedProductsDto> {
+    const take = Number(query.take) || 10; // max products per page
+    const skip = Number(query.skip) || 1; // page number
+
+    const [products, count] = await Promise.all([
+      this.prisma.product.findMany({
+        take,
+        skip: (skip - 1) * take,
+      }),
+      this.prisma.product.count(),
+    ]);
+
+    const pages = Math.ceil(count / take);
+
+    return {
+      products: products.map(productToDto),
+      pages: pages === 0 ? 1 : pages,
+      count,
+    };
   }
 
-  update(id: number, dto: UpdateProductDto) {
-    return this.prisma.product.update({ where: { id }, data: dto });
+  async findOne(id: number): Promise<ResponseGetProductDto> {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+
+    if (!product) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+
+    return productToDto(product);
   }
 
-  remove(id: number) {
-    return this.prisma.product.delete({ where: { id } });
+  async update(id: number, dto: UpdateProductDto): Promise<ResponseUpdateProductDto> {
+    const exists = await this.prisma.product.findUnique({ where: { id } });
+
+    if (!exists) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+
+    const companyExists = await this.prisma.company.findUnique({
+      where: { id: dto.companyId },
+    });
+
+    if (!companyExists) {
+      throw new NotFoundException('Empresa informada não encontrada.');
+    }
+
+    const data = {
+      name: dto.name,
+      description: dto.description,
+      price: dto.price,
+      company: dto.companyId ? { connect: { id: dto.companyId } } : undefined,
+    };
+
+    const updated = await this.prisma.product.update({
+      where: { id },
+      data,
+    });
+
+    return productToDto(updated);
+  }
+
+  async remove(id: number): Promise<ResponseDeleteProductDto> {
+    const product = await this.prisma.product.findUnique({ where: { id } });
+
+    if (!product) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+
+    await this.prisma.product.delete({ where: { id } });
+
+    return { message: 'Produto excluído com sucesso' };
   }
 }
